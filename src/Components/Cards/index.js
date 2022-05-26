6c96a2c5e928c7d5bec0d4c8af3e17906d84986e
JoaoVictorBezerra <joaov-1234@hotmail.com>
2022-05-26 11:41:22 -0300

    EDIT: Cards

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,26 +1,29 @@
 import styles from '../../../styles/Cards/Cards.module.css'
+import Link from '../Links'
 
 export default function Cards() {
-    return(
+    return (
         <div className={styles.grid}>
             <div className={styles.row}>
+                <Link href="https://github.com/JoaoVictorBezerra/mywebsite" target="_blank">
+                    <div className={styles.card}>
+                        <h1>Portfólio João Victor</h1>
+                        <p>Esse projeto foi o primeiro projeto feito por mim, foi construído a partir das linguagens HTML, CSS e Javascript, contando com o framework Next.js</p>
+                    </div>
+                </Link>
                 <div className={styles.card}>
                     <h1>Lorem Ipsum</h1>
-                    <p>aa</p>
-                </div>
-                <div className={styles.card}>
-                    <h1>Lorem Ipsum</h1>
-                    <p>aa</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                 </div>
             </div>
             <div className={styles.row}>
                 <div className={styles.card}>
                     <h1>Lorem Ipsum</h1>
-                    <p>aa</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                 </div>
                 <div className={styles.card}>
                     <h1>Lorem Ipsum</h1>
-                    <p>aa</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                 </div>
             </div>
         </div>
